4e72df71b5d9173a4bfc3bc2add764ca6c1fba74
Kate <katepdonahue@gmail.com>
2013-12-10 12:43:36 -0500

    Add some CSS

--- game.rb
+++ game.rb
@@ -4,13 +4,17 @@ Bundler.require
 module Game
   class RPS_App < Sinatra::Application
 
+    get '/' do
+      erb :index
+    end
+
     get '/throw/:type' do |type|
       @computer_move = ["rock", "paper", "scissors"].sample
       win_array = [ ["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
       @outcome = "You won!" if win_array.include? [type, @computer_move]
       @outcome = "You lost!" if win_array.include? [@computer_move, type]
       @outcome = "Draw." if type == @computer_move
-      erb :index
+      erb :outcome
     end
 
   end
