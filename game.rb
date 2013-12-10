70bc4edbe7da0c6e9e7cef1f876abb696787f21e
Kate <katepdonahue@gmail.com>
2013-12-10 12:02:55 -0500

    Add computer move to outcome screen

--- game.rb
+++ game.rb
@@ -4,12 +4,12 @@ Bundler.require
 module Game
   class RPS_App < Sinatra::Application
 
-    get '/throw/:type' do
-      computer_move = ["rock", "paper", "scissors"].sample
+    get '/throw/:type' do |type|
+      @computer_move = ["rock", "paper", "scissors"].sample
       win_array = [ ["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
-      @outcome = "You won!" if win_array.include? [:type, computer_move]
-      @outcome = "You lost!" if win_array.include? [computer_move, :type]
-      @outcome = "Draw." if :type == computer_move
+      @outcome = "You won!" if win_array.include? [type, @computer_move]
+      @outcome = "You lost!" if win_array.include? [@computer_move, type]
+      @outcome = "Draw." if type == @computer_move
       erb :index
     end
 
