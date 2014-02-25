694f7dc1c58416b9e11ab4a69bfc83c6df7d22d6
Kate <katepdonahue@gmail.com>
2014-02-25 09:16:50 -0500

    Add new images and new css

--- game.rb
+++ game.rb
@@ -12,7 +12,7 @@ module Game
       @computer_move = ["rock", "paper", "scissors"].sample
       win_array = [ ["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
       @outcome = "You won!" if win_array.include? [params[:type], @computer_move]
-      @outcome = "You lost!" if win_array.include? [@computer_move, params[:type]]
+      @outcome = "You lost..." if win_array.include? [@computer_move, params[:type]]
       @outcome = "Draw." if params[:type] == @computer_move
       erb :outcome
     end
